f7a75bd77fa1d483ecf596582e893883cb56f561
NapswySC <dvalenzuela@softcrum.com>
2014-08-29 11:46:46 -0400

    Actualización del generador de aplicación principal

--- app/index.js
+++ app/index.js
@@ -1,83 +1,85 @@
+// Se define el uso de sintaxis Node.js
 'use strict';
-var util = require('util');
-var path = require('path');
-var yeoman = require('yeoman-generator');
-var cgUtils = require('../utils.js');
 
-var CgangularGenerator = module.exports = function CgangularGenerator(args, options, config) {
+// Se definen todos los componentes que se requiren para poder ejecutar la aplicacion
+var util            = require('util');
+var path            = require('path');
+var yeoman          = require('yeoman-generator');
+var frontendUtils   = require('../utils.js');
+
+//
+var FrontendGenerator = module.exports = function FrontendGenerator(args, options, config) {
     yeoman.generators.Base.apply(this, arguments);
-
     this.on('end', function () {
-        this.config.set('partialDirectory','partial/');
-        this.config.set('directiveDirectory','directive/');
-        this.config.set('filterDirectory','filter/');
-        this.config.set('serviceDirectory','service/');
+        this.config.set('partialDirectory',     'partial/');
+        this.config.set('directiveDirectory',   'directive/');
+        this.config.set('filterDirectory',      'filter/');
+        this.config.set('serviceDirectory',     'service/');
         var inject = {
             js: {
-                file: 'index.html',
-                marker: cgUtils.JS_MARKER,
-                template: '<script src="<%= filename %>"></script>'
+                file:               'index.html',
+                marker:             frontendUtils.JS_MARKER,
+                template:           '<script src="<%= filename %>"></script>'
             },
             less: {
-                relativeToModule: true,
-                file: '<%= module %>.less',
-                marker: cgUtils.LESS_MARKER,
-                template: '@import "<%= filename %>";'
+                relativeToModule:   true,
+                file:               '<%= module %>.less',
+                marker:             frontendUtils.LESS_MARKER,
+                template:           '@import "<%= filename %>";'
             }
         };
-        this.config.set('inject',inject);
+        this.config.set('inject', inject);
         this.config.save();
         this.installDependencies({ skipInstall: options['skip-install'] });
     });
-
-    this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+    this.pkg    = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
-util.inherits(CgangularGenerator, yeoman.generators.Base);
+//
+util.inherits(FrontendGenerator, yeoman.generators.Base);
 
-CgangularGenerator.prototype.askFor = function askFor() {
+//
+FrontendGenerator.prototype.askFor = function askFor() {
     var cb = this.async();
-
     var prompts = [{
-        name: 'appname',
-        message: 'What would you like the angular app/module name to be?',
-        default: path.basename(process.cwd())
+        name:       'appname',
+        message:    '¿Qué nombre tendrá el proyecto?',
+        default:    path.basename(process.cwd())
     }];
-
     this.prompt(prompts, function (props) {
-        this.appname = props.appname;
+        this.appname    = props.appname;
         cb();
     }.bind(this));
 };
 
-CgangularGenerator.prototype.askForUiRouter = function askFor() {
-    var cb = this.async();
-
+//
+FrontendGenerator.prototype.askForUiRouter = function askFor() {
+    var cb      = this.async();
     var prompts = [{
-        name: 'router',
-        type:'list',
-        message: 'Which router would you like to use?',
-        default: 0,
-        choices: ['Standard Angular Router','Angular UI Router']
+        name:       'router',
+        type:       'list',
+        message:    '¿Qué componente de ruteo desea ocupar en el proyecto?',
+        default:    0,
+        choices:    ['Standard Angular Router', 'Angular UI Router']
     }];
-
     this.prompt(prompts, function (props) {
         if (props.router === 'Angular UI Router') {
-            this.uirouter = true;
-            this.routerJs = 'bower_components/angular-ui-router/release/angular-ui-router.js';
-            this.routerModuleName = 'ui.router';
-            this.routerViewDirective = 'ui-view';
+            this.uirouter               = true;
+            this.routerJs               = 'bower_components/angular-ui-router/release/angular-ui-router.js';
+            this.routerModuleName       = 'ui.router';
+            this.routerViewDirective    = 'ui-view';
         } else {
-            this.uirouter = false;
-            this.routerJs = 'bower_components/angular-route/angular-route.js';
-            this.routerModuleName = 'ngRoute';
-            this.routerViewDirective = 'ng-view';
+            this.uirouter               = false;
+            this.routerJs               = 'bower_components/angular-route/angular-route.js';
+            this.routerModuleName       = 'ngRoute';
+            this.routerViewDirective    = 'ng-view';
         }
         this.config.set('uirouter',this.uirouter);
         cb();
     }.bind(this));
 };
 
-CgangularGenerator.prototype.app = function app() {
-    this.directory('skeleton/','./');
+//
+FrontendGenerator.prototype.app = function app() {
+    this.directory('skeleton/', './');
 };
